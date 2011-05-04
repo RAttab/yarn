def1c1958b88559591265ab8fea6476be7d1f27d
Rémi Attab <attab.remi@gmail.com>
2011-05-04 00:40:53 -0400

    Added exported fallbacks to alloc.h inline functions.

---
+++ libyarn/src/alloc.c
@@ -0,0 +1,16 @@
+/*!
+\author Rémi Attab
+\license FreeBSD (see LICENSE file)
+
+\brief 
+
+
+ */
+
+
+#include "alloc.h"
+
+
+extern inline void* yarn_malloc(size_t size);
+extern inline void* yarn_memalign(size_t alignment, size_t size);
+extern inline void yarn_free (void* ptr);
