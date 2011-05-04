ef89a34ed2d3ffcb4e691c7c6f99c0be7c862062
Rémi Attab <attab.remi@gmail.com>
2011-05-04 16:46:35 -0400

    Removed left-over struct keywords.

--- libyarn/src/atomic.c
+++ libyarn/src/atomic.c
@@ -10,35 +10,37 @@ inline or if the user tries to grab the address.
 
 
 
-extern inline yarn_atomv_t yarn_readv(struct yarn_atomic_var* a);
-extern inline void* yarn_readp(struct yarn_atomic_ptr* a);
+extern inline yarn_atomv_t yarn_readv(yarn_atomic_var* a);
+extern inline void* yarn_readp(yarn_atomic_ptr* a);
 
-extern inline yarn_atomv_t yarn_readv_barrier(struct yarn_atomic_var* a);
-extern inline void* yarn_readp_barrier(struct yarn_atomic_ptr* a);
+extern inline yarn_atomv_t yarn_readv_barrier(yarn_atomic_var* a);
+extern inline void* yarn_readp_barrier(yarn_atomic_ptr* a);
 
-extern inline void yarn_writev(struct yarn_atomic_var* a, yarn_atomv_t var);
-extern inline void yarn_writep(struct yarn_atomic_ptr* a, void* ptr);
+extern inline void yarn_writev(yarn_atomic_var* a, yarn_atomv_t var);
+extern inline void yarn_writep(yarn_atomic_ptr* a, void* ptr);
 
 extern inline void yarn_writev_barrier(yarn_atomic_var* a, yarn_atomv_t var);
 extern inline void yarn_writep_barrier(yarn_atomic_ptr* a, yarn_atomp_t ptr);
 
-extern inline yarn_atomv_t yarn_incv(struct yarn_atomic_var* a);
-extern inline yarn_atomp_t yarn_decv(struct yarn_atomic_var* a);
+extern inline yarn_atomv_t yarn_incv(yarn_atomic_var* a);
+extern inline yarn_atomp_t yarn_decv(yarn_atomic_var* a);
 
 
-extern inline yarn_atomv_t yarn_casv (struct yarn_atomic_var* a, 
+extern inline yarn_atomv_t yarn_casv (yarn_atomic_var* a, 
 				      yarn_atomv_t oldval, 
 				      yarn_atomv_t newval);
-extern inline yarn_atomp_t yarn_casp (struct yarn_atomic_ptr* a,
+
+extern inline yarn_atomp_t yarn_casp (yarn_atomic_ptr* a,
 				      void* oldval,
 				      void* newval);
 
-extern inline yarn_atomv_t yarn_casv_fast (struct yarn_atomic_var* a,
+extern inline yarn_atomv_t yarn_casv_fast (yarn_atomic_var* a,
 					   yarn_atomv_t oldval, 
-					   yarn_atomv_t newval) 
-extern inline yarn_atomp_t yarn_casp_fast (struct yarn_atomic_ptr* a,
+					   yarn_atomv_t newval);
+ 
+extern inline yarn_atomp_t yarn_casp_fast (yarn_atomic_ptr* a,
 					   void* oldval, 
-					   void* newval) 
+					   void* newval);
 
 
 
