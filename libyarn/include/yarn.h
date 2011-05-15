9f5920e0708050cf309232dc5fa59e5c948755ac
Rémi Attab <attab.remi@gmail.com>
2011-05-14 23:35:43 -0400

    Added the sole unified user header.

---
+++ libyarn/include/yarn.h
@@ -0,0 +1,15 @@
+/*!
+\author Rémi Attab
+\license FreeBSD (see the LICENSE file)
+
+The main yarn header for the target programs.
+ */
+
+#ifndef YARN_YARN_H_
+#define YARN_YARN_H_
+
+
+#include "dependency.h"
+
+
+#endif // YARN_YARN_H_
