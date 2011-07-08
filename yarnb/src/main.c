e0faafee6af6e0c30e0426160cea483d87bda067
Rémi Attab <attab.remi@gmail.com>
2011-07-08 14:58:27 -0400

    Various fixes and "improvments" to the benchmarking tests.

--- yarnb/src/main.c
+++ yarnb/src/main.c
@@ -1,6 +1,14 @@
 /*!
 \author Rémi Attab
 \license FreeBSD (see the LICENSE file).
+
+Before you say anything:
+- Yes, this code is crap
+- Yes, I need to rewrite it
+- Yes, I will do it in the near future
+- No, I'm not just saying that
+- Yes, I really mean it
+Now leave me alone...
  */
 
 #include <yarn.h>
@@ -15,6 +23,8 @@
 struct task {
   size_t i;
   size_t n;
+
+  yarn_word_t thread_count;
   
   yarn_time_t wait_time;
 
@@ -22,9 +32,6 @@ struct task {
   yarn_word_t array[];
 
 };
-
-#define DEFAULT_ARRAY_SIZE 16
-#define DEFAULT_N (DEFAULT_ARRAY_SIZE * DEFAULT_ARRAY_SIZE)
 
 #define TIME_END_NS 1000000
 #define TIME_STEP_NS 100
@@ -37,15 +44,13 @@ struct task {
 #define WARN  "WARN  - "
 #define ERROR "ERROR - "
 
-double get_speedup(yarn_time_t wait_time);
+double get_speedup(yarn_time_t wait_time, yarn_word_t thread_count);
 
 int speedup_search (const double target_speedup,
-		     const yarn_time_t start_time, 
-		     const yarn_time_t end_time, 
-		     yarn_time_t* speedup_time);
-yarn_time_t speedup_lower_bound (const double start_speedup, 
-				 const yarn_time_t start_time, 
-				 const yarn_time_t step);
+		    const yarn_time_t start_time, 
+		    const yarn_time_t end_time, 
+		    yarn_time_t* speedup_time, 
+		    yarn_word_t thread_count);
 
 void run_normal (struct task* t);
 enum yarn_ret run_speculative (yarn_word_t pool_id, void* task);
@@ -87,63 +92,59 @@ int main (int argc, char** argv) {
   // Get from command line?
   yarn_time_t start_time = 0;
   const yarn_time_t end_time = TIME_END_NS;
-  const yarn_time_t time_step = TIME_STEP_NS;
 
   const double min_speedup = SPEEDUP_MIN;
-  const double max_speedup = yarn_thread_count();
   const double speedup_step = SPEEDUP_STEP;
 
-  printf(INFO "Yarn benchmark tests.\n");
+  printf(INFO "Yarn Benchmark\n");
   printf(INFO "\tSpeculative threads = %zu\n", yarn_thread_count());
-
-  printf(INFO "\tSpeedup range = [%f, %f]\n", min_speedup, max_speedup);
-  printf(INFO "\tSpeedup delta = %f\n", speedup_step);
-
   printf(INFO "\tTime range = [0ns, %zuns]\n", end_time);
-  printf(INFO "\tTime delta = %zuns\n", time_step);
+  printf(INFO "\tSpeedup min = %2.2f\n", min_speedup);
+  printf(INFO "\tSpeedup delta = %2.2f\n", speedup_step);
 
-
+  printf(INFO "\n");
   printf(INFO "Warming up...\n");
+  fflush(stdout);
   for (int i = 0; i < 10; ++i) {
-    (void) get_speedup(0);
+    (void) get_speedup(0, 1);
   }
 
-  printf(INFO "Getting bounds...\n");
-  g_max_speedup = get_speedup(end_time);
-  g_min_speedup = get_speedup(start_time);
-  printf(INFO "\tMIN_TIME=%zu => SPEEDUP=%f\n", start_time, g_min_speedup);
-  printf(INFO "\tMAX_TIME=%zu => SPEEDUP=%f\n", end_time, g_max_speedup);
 
+  printf(INFO "\n");
   printf(INFO "Executing the benchmark...\n");
-  double best_speedup = 0.0;
-  for (double speedup = min_speedup; speedup <= max_speedup; speedup += speedup_step) {
+  fflush(stdout);
+ 
+  for (yarn_word_t threads = 1; threads <= yarn_thread_count(); ++threads) {
+    start_time = 0;
 
-    yarn_time_t speedup_time;
-    int ret = speedup_search(speedup, start_time, end_time, &speedup_time);
-    if (ret > 0) {
-      break;
+    g_max_speedup = get_speedup(end_time, threads);
+    g_min_speedup = get_speedup(start_time, threads);
+    printf(INFO "(%2zu / %2zu) = [%3f, %3f]\n", 
+	   threads, yarn_thread_count(), g_min_speedup, g_max_speedup);    
+    fflush(stdout);
+
+    // Theorical limit for speedups is the number of threads involved.
+    const double max_speedup = threads;
+    
+    for (double speedup = min_speedup; speedup <= max_speedup; speedup += speedup_step) {
+      yarn_time_t speedup_time;
+      int ret = speedup_search(speedup, start_time, end_time, &speedup_time, threads);
+      if (ret > 0) {
+	break;
+      }
+      else if (ret < 0) {
+	printf(INFO "(%2zu, %2.2f) = SKIPPED\n", threads, speedup);
+	fflush(stdout);
+	continue;
+      }
+
+      //    speedup_time = speedup_lower_bound(speedup, start_time, time_step);
+      start_time = speedup_time;
+
+      printf(INFO "(%2zu, %2.2f) = %9zuns\n", threads, speedup, speedup_time);
+      fflush(stdout);
     }
-    else if (ret < 0) {
-      printf(INFO "SPEEDUP=%f => SKIPPED (too small)\n", speedup);
-      best_speedup = speedup;
-      continue;
-    }
-
-    //    speedup_time = speedup_lower_bound(speedup, start_time, time_step);
-    start_time = speedup_time;
-
-    printf(INFO "SPEEDUP=%f => TIME=%zuns\n", speedup, speedup_time);
-    best_speedup = speedup;
   }
-
-
-  if (comp_speedup(best_speedup, min_speedup) >= 0) {
-    printf(INFO "Best speedup achieved => %f\n", best_speedup);
-  }
-  else {
-    printf(WARN "Unable to achieve minimal speedup => %f\n", min_speedup);
-  }
-
 
   yarn_destroy();
   return 0;
@@ -155,21 +156,22 @@ int main (int argc, char** argv) {
 }
 
 int speedup_search (const double target_speedup,
-		     yarn_time_t start_time, 
-		     yarn_time_t end_time, 
-		     yarn_time_t* speedup_time) 
+		    yarn_time_t start_time, 
+		    yarn_time_t end_time, 
+		    yarn_time_t* speedup_time,
+		    yarn_word_t thread_count) 
 {
-  printf(DEBUG "searching for => %f\n", target_speedup);
+  //  printf(DEBUG "searching for => %f\n", target_speedup);
 
   double start_speedup = g_min_speedup;
   if (comp_speedup(start_speedup, target_speedup) > 0) {
-    printf(WARN "Target is below start_time (start_speedup=%f)\n", start_speedup);
+    printf(WARN "target=%3f < min=%3f)\n", target_speedup, start_speedup);
     return -1;
   }
 
   double end_speedup = g_max_speedup;
   if (comp_speedup(end_speedup, target_speedup) < 0) {
-    printf(WARN "Target is above end_time (end_speedup=%f)\n", end_speedup);
+    printf(WARN "target=%3f > max=%3f)\n", target_speedup, end_speedup);
     return 1;
   }
 
@@ -180,20 +182,17 @@ int speedup_search (const double target_speedup,
   int speedup_comp;
   do {
 
-    double slope = 
-      ((double) end_speedup - start_speedup) / 
-      ((double) end_time - start_time);
-
-    time = (target_speedup - start_speedup) / slope;
+    double slope = (end_speedup - start_speedup) / ((double)(end_time - start_time));
+    time = start_time + ((target_speedup - start_speedup) / slope);
 
     if (time > TIME_END_NS) {
       time = TIME_END_NS;
     }
-
+    /*
     printf(DEBUG "search - time=%zu, slope=%f (speedup=[%f, %f], time=[%zu, %zu])\n", 
 	   time, slope, start_speedup, end_speedup, start_time, end_time); 
-
-    speedup = get_speedup(time);
+    */
+    speedup = get_speedup(time, thread_count);
     
     speedup_comp = comp_speedup(speedup, target_speedup);
     if (speedup_comp < 0) {
@@ -207,51 +206,28 @@ int speedup_search (const double target_speedup,
 
   } while (speedup_comp != 0);
 
-  printf(DEBUG "Target time => %zuns\n", time);
+  //printf(DEBUG "Target time => %zuns\n", time);
 
   *speedup_time = time;
   return 0;
 }
 
-yarn_time_t speedup_lower_bound (const double start_speedup, 
-				 const yarn_time_t start_time, 
-				 const yarn_time_t step) 
-{
-  printf(DEBUG "lower_bound for speedup => %f\n", start_speedup);
-
-  double speedup;
-  yarn_time_t prev_time;
-  yarn_time_t time = start_time;
-
-  do {
-    prev_time = time;
-    time -= step;    
-
-    if (prev_time < step) {
-      break;
-    }
-
-    printf(DEBUG "lower_bound it=%zu\n", time);
-
-    speedup = get_speedup(time);
-    
-  } while (comp_speedup(speedup, start_speedup) == 0);
-
-  printf(DEBUG "lower_bound time => %zuns\n", prev_time);
-
-  return prev_time;
-}
 
 
-struct task* create_task (size_t array_size, size_t n, yarn_time_t wait_time) {
+struct task* create_task (yarn_time_t wait_time, yarn_word_t thread_count) {
+  size_t array_size = yarn_thread_count();
+
   size_t t_size = sizeof(struct task) + sizeof(yarn_word_t)*array_size;
   struct task* t = (struct task*) malloc(t_size);
   if (!t) goto alloc_error;
 
+
   t->i = 0;
-  t->n = n;
   t->wait_time = wait_time;
+  t->thread_count = thread_count;
   t->array_size = array_size;
+  t->n = t->array_size * t->array_size;
+
   for (size_t i = 0; i < array_size; ++i) {
     t->array[i] = 0;
   }
@@ -273,12 +249,15 @@ void exec_normal (struct task* t) {
 
 void exec_speculative (struct task* t) {
   bool ret = yarn_exec_simple(run_speculative, (void*) t, 
-			      YARN_ALL_THREADS, t->array_size, 0);
+			      t->thread_count, t->array_size, 0);
   assert(ret);
 }
 
 
-yarn_time_t time_exec (exec_func_t exec_func, yarn_time_t wait_time) {
+yarn_time_t time_exec (exec_func_t exec_func, 
+		       yarn_time_t wait_time, 
+		       yarn_word_t thread_count) 
+{
   static const int n = 10;
 
   yarn_time_t time_sum = 0;
@@ -286,7 +265,7 @@ yarn_time_t time_exec (exec_func_t exec_func, yarn_time_t wait_time) {
   yarn_time_t max_time = 0;
 
   for (int i = 0; i < n; ++i) {
-    struct task* t = create_task(DEFAULT_ARRAY_SIZE, DEFAULT_N, wait_time);
+    struct task* t = create_task(wait_time, thread_count);
     yarn_time_t start = yarn_timer_sample_system();
 
     (*exec_func)(t);
@@ -314,16 +293,17 @@ yarn_time_t time_exec (exec_func_t exec_func, yarn_time_t wait_time) {
 
 
 // Could rerun each a couple of time to make sure that we're within our epsilon.
-double get_speedup(yarn_time_t wait_time) {
+double get_speedup(yarn_time_t wait_time, yarn_word_t thread_count) {
   assert(wait_time <= TIME_END_NS);
 
-  yarn_time_t base_time = time_exec(exec_normal, wait_time);
-  yarn_time_t speculative_time = time_exec(exec_speculative, wait_time);
+  yarn_time_t base_time = time_exec(exec_normal, wait_time, thread_count);
+  yarn_time_t speculative_time = time_exec(exec_speculative, wait_time, thread_count);
 
   double speedup = (double) base_time / (double) speculative_time;
-
+  /*
   printf(DEBUG "time=%zu => speedup=%f (base=%zu, spec=%zu)\n", 
 	 wait_time, speedup, base_time, speculative_time);
+  */
 
   return speedup;
 }
